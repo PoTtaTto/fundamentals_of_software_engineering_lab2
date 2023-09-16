59142ee41fcce6581525ef5e047f8895523300d5
MrPotatsus <MrPotatsus@yandex.ru>
2023-09-16 17:59:03 +0300

    Car class is added

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import uuid
+import enum
+
+
 class Car:
     def __init__(self, is_for_training: bool, max_speed: float):
         self.is_for_training = is_for_training
@@ -8,3 +12,10 @@ class Car:
             return f'Training car with max speed - {self.max_speed}km/h'
         else:
             return f'Normal car with max speed - {self.max_speed}'
+
+
+class Driver:
+    def __init__(self, driving_skill: float):
+        self.id = str(uuid.uuid4())
+        self.driving_skill = driving_skill
+
